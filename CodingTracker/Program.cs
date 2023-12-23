d33ca557f82ea72b0fce2845b8f9c7f7a1607642
diaolosh <dionchristou@gmail.com>
2023-12-23 20:41:26 +0200

    removed "multiple"

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy more");
-Console.WriteLine("Hello, C# Academy more");
-Console.WriteLine("Hello, C# Academy more");
-Console.WriteLine("Hello, C# Academy more");
